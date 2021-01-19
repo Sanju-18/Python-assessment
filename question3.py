3e64ab2330564bdc957173f78e7c41e28a698bc4
Sanjana Shuruthy K <sanjanashuruthy18@gmail.com>
2021-01-19 23:36:57 +0530

    assessment1

--- question3.py
+++ question3.py
@@ -1,6 +1,10 @@
 import random
+# getting input
 value = input()
+
+#generating random value
 n = random.randint(1,26)
 
+#inserting random alphabet and reversing the input string
 new_value = chr(64+n)+value[::-1].lower()
 print(new_value)
